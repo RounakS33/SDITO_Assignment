f01d63a91a56b90b9774c221af7ac75df2f49ba9
Rounak Singh <108825280+RounakS33@users.noreply.github.com>
2025-04-03 11:10:22 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('Tumi mosto boro Bhudu')
 })
 
 app.get('/courses', function (req, res) {
